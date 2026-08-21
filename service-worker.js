124af7e844724b1cf680fbdcb62d67c6b3587283
pharmacopee-keneyaso <Keitabefo92@gmail.com>
2026-08-21 12:34:24 +0000

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,28 +1,18 @@
 const CACHE_NAME = 'keneyaso-v1';
-const ASSETS = [
-  './',
-  './index.html',
-  './manifest.json',
-  './icon-192.png',
-  './icon-512.png'
-];
-
+const ASSETS = ['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
   self.skipWaiting();
 });
-
 self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-    )
-  );
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))));
   self.clients.claim();
 });
-
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    fetch(event.request).catch(() => caches.match(event.request))
-  );
+  if (event.request.method !== 'GET') return;
+  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request).then(resp => {
+    const copy = resp.clone();
+    caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+    return resp;
+  }).catch(() => caches.match('./index.html'))));
 });
